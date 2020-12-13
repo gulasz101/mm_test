ee7b18f96b10e6bb516b8dc4bf7a8eddbda78e08
wojciechgula <gulasz101@gmail.com>
2020-12-13 20:53:17 +0100

    Installing easy coding standard to newest version due to "prefixed".

--- ecs.php
+++ ecs.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+//config for 9+
 use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\CodingStandard\Fixer\LineLength\LineLengthFixer;
@@ -29,3 +30,36 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ]
     );
 };
+
+/*
+// config for 8+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return function (ContainerConfigurator $containerConfigurator): void
+{
+    $containerConfigurator->import(__DIR__ . '/vendor/symplify/easy-coding-standard/config/set/clean-code.php');
+    $containerConfigurator->import(__DIR__ . '/vendor/symplify/easy-coding-standard/config/set/dead-code.php');
+    $containerConfigurator->import(__DIR__ . '/vendor/symplify/easy-coding-standard/config/set/psr12.php');
+    $containerConfigurator->import(__DIR__ . '/vendor/symplify/easy-coding-standard/config/set/php70.php');
+    $containerConfigurator->import(__DIR__ . '/vendor/symplify/easy-coding-standard/config/set/php71.php');
+
+    $services = $containerConfigurator->services();
+
+    $services->set(\SlevomatCodingStandard\Sniffs\Variables\UselessVariableSniff::class);
+    $services->set(\SlevomatCodingStandard\Sniffs\Functions\UnusedInheritedVariablePassedToClosureSniff::class);
+    $services->set(\SlevomatCodingStandard\Sniffs\PHP\UselessSemicolonSniff::class);
+    $services->set(\SlevomatCodingStandard\Sniffs\PHP\UselessParenthesesSniff::class);
+    $services->set(\PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer::class)
+        ->call('configure', [['syntax' => 'short']]);
+    $services->set(\PhpCsFixer\Fixer\Import\NoUnusedImportsFixer::class);
+    $services->set(\PhpCsFixer\Fixer\Import\OrderedImportsFixer::class);
+    $services->set(\PhpCsFixer\Fixer\Semicolon\NoEmptyStatementFixer::class);
+    $services->set(\PhpCsFixer\Fixer\ClassNotation\ProtectedToPrivateFixer::class);
+    $services->set(\PhpCsFixer\Fixer\ControlStructure\NoUnneededControlParenthesesFixer::class);
+    $services->set(\PhpCsFixer\Fixer\ControlStructure\NoUnneededCurlyBracesFixer::class);
+    $services->set(\SlevomatCodingStandard\Sniffs\ControlStructures\RequireShortTernaryOperatorSniff::class);
+    $services->set(\SlevomatCodingStandard\Sniffs\Operators\RequireCombinedAssignmentOperatorSniff::class);
+
+    $containerConfigurator->parameters();
+};
+*/
